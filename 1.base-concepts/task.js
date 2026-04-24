175ad65c0379fa70787c7a1379a6d70166f42648
Andrey <Andrey_Alecseev_RU@icloud.com>
2026-04-24 16:41:19 +0300

    Task_1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+function solveEquation(coefficientA, coefficientB, coefficientC) {
+    let roots = [];
+    const discriminant = coefficientB ** 2 - 4 * coefficientA * coefficientC;
+
+    if (discriminant === 0) {
+        roots.push(-coefficientB / (2 * coefficientA));
+    } else if (discriminant > 0) {
+        roots.push((-coefficientB + Math.sqrt(discriminant)) / (2 * coefficientA));
+        roots.push((-coefficientB - Math.sqrt(discriminant)) / (2 * coefficientA));
+    }
+
+    return roots;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function calculateTotalMortgage(annualPercent, contribution, totalCost, durationMonths) {
+    let monthlyRate = (annualPercent / 100) / 12;
+    let loanBody = totalCost - contribution;
+    let monthlyPayment = loanBody * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, durationMonths) - 1)));
+    let totalAmount = monthlyPayment * durationMonths;
+
+    return Number(totalAmount.toFixed(2));
 }
